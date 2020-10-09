3d6ec0c9ab92922c26cd17e3935195341edf99ea
Stephen Sheng <stevexsheng@gmail.com>
2020-10-09 03:53:01 -0700

    added implementation for Larry and Valerie's mobility model

---
+++ models/lv_mobility/model.py
@@ -0,0 +1,82 @@
+import numpy
+import scipy
+from scipy.stats import gamma, norm
+
+
+class lv_mobility:
+    """
+    An implementation of Larry and Valerie's mobility model 
+
+    Attributes:
+        args (dict): A dictionary containing values for A, alpha, beta, mu, and sig. Additional parameters can be included as well.
+        fit: A method for fitting model parameters
+        eval: A method for evaluating the model at timestep t
+        batch_eval: A method for evaluation the model at timesteps between t1 and t2, inclusive.
+    """
+
+    def __init__(self, args={}):
+        """Inits model parameters"""
+        self.args = args
+
+    def fit(self, M, DC, y_true, optimizer, loss, args):
+        """
+        Method for fitting model parameters
+
+        Args:
+            M (numpy array, shape=(num_features, num_intervals)): mobility time series
+            DC (numpy array, shape=(num_intervals)): deatch curve
+            y_true: observed values
+            optimizer: optimizer for model
+            loss: loss function
+            args (dict): keys - ALPHA, BETA, MU : values - list of values for initialization
+        """
+        optimizer(self, M, DC, y_true, loss, args)
+
+    def _eval(self, M, DC, L, A, alpha, beta, mu, sig):
+        """
+        Evaluation method for optimization
+
+        Args:
+            M (numpy array, shape=(num_intervals)): mobility time series
+            DC (numpy array, shape=(num_intervals)): death curve 
+            L (int): length of interval you want to evaluate over
+            A (float): 
+            alpha (float): b_0 term in Larry and Valerie's doc
+            beta (float): b_1 term in Larry and Valerie's doc
+            mu (float): mean for normal distribution  in model
+            sig (float): std for normal distribution in model
+
+        Returns:
+            A numpy array containing the predictions of the model between timesteps
+            1 and L
+        """
+        Beta = beta + alpha * \
+            norm.cdf(M[0:L], loc=abs(mu), scale=abs(sig))
+        BetaSum = numpy.cumsum(numpy.insert(Beta, 0, 0))
+        BetaSum = numpy.delete(BetaSum, L)
+
+        # TODO(Stephen, Shitong): The initialization for the out variable needs to be tuned.
+        # It fits poorly at beginning if A is large if all terms are nonzero
+        # But it fits better at later timesteps with nonzero initialization
+        # These simple initializations work decently but aren't optimal
+        #out = (1/L) * numpy.ones(L)
+        out = numpy.zeros(L)
+        DC0 = DC
+        for d in range(L):
+            DC0 = numpy.insert(DC0, 0, 0)
+            DC0 = numpy.delete(DC0, L)
+            out = out + DC0*numpy.exp(BetaSum[d])
+
+        return abs(A)*out
+
+    def eval(self, M, DC, t, A, beta, alpha, mu, sig):
+        """
+        Method for evaluating the model at a specific timestep
+        """
+        assert(NotImplemented)
+
+    def batch_eval(self, M, DC, t1, t2, A, beta, alpha, mu, sig):
+        """
+        Method for evaluating the model in a range of timesteps
+        """
+        assert(NotImplemented)
---
+++ models/lv_mobility/optimizer.py
@@ -0,0 +1,67 @@
+import numpy
+from scipy.optimize import dual_annealing
+import itertools
+
+
+def optim(model, M, DC, y_true, loss, args=None):
+    """
+    A method for constrained optimization using dual_annealing from scipy.optimize for lv_mobility.
+
+    Args:
+        M (numpy array): mobility curve
+        DC (numpy array): death curve
+        y_true (numpy array): observed value of cases
+        loss: loss function 
+        args (dict): ALPHA, BETA, MU, array of parameters for initialization
+    """
+    if args == None:
+        ALPHA = numpy.linspace(-2, 2, num=6)
+        BETA = numpy.linspace(-2, 2, num=6)
+        MU = numpy.linspace(0.1, 0.5, num=5)
+    else:
+        ALPHA = args["ALPHA"]
+        BETA = args["BETA"]
+        MU = args["MU"]
+
+    iterator = itertools.product(
+        range(len(ALPHA)),
+        range(len(BETA)),
+        range(len(MU)),
+    )
+
+    def loss_wrap(x, *args):
+        y_pred = model._eval(M, DC, L, x[0], x[1], x[2], x[3], x[4])
+        return loss(y_true, y_pred)
+
+    best_loss = float("inf")
+    L = len(M)
+
+    for i, j, k in iterator:
+        par_init = numpy.array(
+            [1.5+numpy.mean(y_true)/L, ALPHA[i], BETA[j], MU[k], 1])
+        bounds = numpy.array([
+            [-100, 100],
+            [-4, 4],
+            [-40, 40],
+            [-2, 2],
+            [0.00001, 2],
+        ])
+
+        args_ = [M, DC, L]
+        res = dual_annealing(
+            func=loss_wrap,
+            args=args_,
+            bounds=bounds,
+            x0=par_init,
+            initial_temp=20000,
+            visit=2.0,
+            maxiter=200,
+        )
+
+        if res["fun"] < best_loss:
+            best_loss = res["fun"]
+            model.args["A"] = res["x"][0]
+            model.args["alpha"] = res["x"][1]
+            model.args["beta"] = res["x"][2]
+            model.args["mu"] = res["x"][3]
+            model.args["sig"] = res["x"][4]
