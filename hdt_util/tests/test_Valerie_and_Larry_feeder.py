b164acc89005259bc053baa072a5a38dc4fc612e
Lifeng Wei <lfwei@ucdavis.edu>
2020-10-15 11:06:53 -0700

    add test for average pooling for Valerie and Larry mobility model

--- hdt_util/tests/test_Valerie_and_Larry_feeder.py
+++ hdt_util/tests/test_Valerie_and_Larry_feeder.py
@@ -1,6 +1,8 @@
 import hdt_util
-from datetime import date
+from datetime import date, timedelta
 import covidcast
+
+import numpy as np
 
 class Valerie_and_Larry_tester:
     
@@ -53,15 +55,53 @@ class Valerie_and_Larry_tester:
                                                forecast_date=end_date,
                                                geo_type=level)
             direct_mobility = direct_mobility[['geo_value', 'time_value', 'value']]
-            assert direct_mobility.shape == mobility.shape
+            assert direct_mobility.shape[0] == mobility.shape[0]
+            assert direct_mobility.shape[1] == mobility.shape[1] - 1
             for i in range(mobility.shape[0]):
-                for j in range(mobility.shape[1]):
+                for j in range(mobility.shape[1] - 1):
                     val1 = direct_mobility.iloc[i, j]
                     val2 = mobility.iloc[i, j]
                     assert  val1 == val2, 'at location {}, {} != {}'.format((i, j), val1, val2)
                     
-    def test_data_processing(self, period):
-        print('Processing details are still under discussing. The test is considered passed for now')
+    def test_avg_pooling(self, source, signal, start_date, end_date, level, count, cumulated, mobility_level, period):
+        cases, mobility = self.feeder.get_data(source=source, 
+                                               signal=signal, 
+                                               start_date=start_date,
+                                               end_date=end_date, 
+                                               level=level, 
+                                               count=count, 
+                                               cumulated=cumulated,
+                                               mobility_level=mobility_level)
+        
+        avg_cases = self.feeder.average_pooling(cases, period, end_date+timedelta(days=10))
+        avg_mobility = self.feeder.average_pooling(mobility, period, end_date+timedelta(days=10))
+        
+        end_date = end_date + timedelta(days=10)
+        max_time = avg_cases['time'].max()
+        for geo in avg_cases['geo_value'].unique():
+            for time in avg_cases['time'].unique():
+                temp_start_date = end_date - timedelta(days=int(period*(max_time-time) + period - 1))
+                temp_end_date = end_date - timedelta(days=int(period*(max_time-time)))
+                temp_cases = cases[cases['date'].apply(lambda x : temp_start_date <= x <= temp_end_date)]
+                temp_mobility = mobility[mobility['date'].apply(lambda x : temp_start_date <= x <= temp_end_date)]
+                
+                if temp_cases.shape[0] > 0:
+                    value1 = avg_cases[(avg_cases['geo_value']==geo)&(avg_cases['time']==time)]['case_value'].values[0]
+                    value2 = np.mean(temp_cases[temp_cases['geo_value']==geo]['case_value'])
+                    assert abs(value1 - value2) < 1e-4, '{}, {}, {}'.format(value1, value2, abs(value1-value2))
+                else:
+                    assert avg_cases[(avg_cases['geo_value']==geo)&(avg_cases['time']==time)].isnull()['case_value'].values[0]
+                
+                if temp_mobility.shape[0] > 0:
+                    value1 = avg_mobility[(avg_mobility['geo_value']==geo)&(avg_mobility['time']==time)]['mobility_value'].values[0]
+                    value2 = temp_mobility[temp_mobility['geo_value']==geo]['mobility_value'].values
+                    n = value2.shape[0]
+                    value2 = n/period*np.mean(value2) + (period-n)/period*value2[-1]
+                    assert abs(value1 - value2) < 1e-4, '{}, {}, {}'.format(value1, value2, abs(value1-value2))
+                else:
+                    value1 = avg_mobility[(avg_mobility['geo_value']==geo)&(avg_mobility['time']==time)]['mobility_value'].values[0]
+                    value2 = mobility[(mobility['geo_value']==geo)&(mobility['date']==mobility['date'].max())]['mobility_value'].values[0]
+                    assert abs(value1 - value2) < 1e-4, '{}, {}, {}'.format(value1, value2, abs(value1-value2))
         
     def test_area_select(self, area):
         print('Processing details are still under discussing. The test is considered passed for now')
@@ -76,12 +116,19 @@ if __name__ == '__main__':
     count=True
     cumulated=False
     mobility_level=1
+    period_choices = [3, 4, 7]
     
     tester = Valerie_and_Larry_tester(cache_loc)
+    print('Testing data loading')
     tester.test_data_loading(source, signal, start_date, end_date, level, count, cumulated, mobility_level)
-    tester.test_data_processing(4)
+    print('Data loading finished')
+    for period in period_choices:
+        print('Testing period {}'.format(period))
+        tester.test_avg_pooling(source, signal, start_date, end_date, level, count, cumulated, mobility_level, period)
+        print('Period {} finished'.format(period))
+    print('Testing selection of area')
     tester.test_area_select('CA')
-    
+    print('Area selection finished')
     print('Passed All Tests')
 
     
