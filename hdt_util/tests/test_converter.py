d0d943a37d9ba3e8e63edd6a761a86c71a35de7d
Stephen Sheng <stevexsheng@gmail.com>
2020-12-02 01:50:43 -0800

    added assert statement for testing

--- hdt_util/tests/test_converter.py
+++ hdt_util/tests/test_converter.py
@@ -1,9 +1,14 @@
 import pandas as pd
 from hdt_util.converter import address_to_census
 
+
 def test_converter():
     df = pd.read_csv("tests/test_data/CensusBlockGroupTest.csv")
     df.Zip = df.Zip.astype(str)
     for i in range(len(df)):
         if df.iloc[i].GEOID_census_block_group != "not found":
             address = ",".join(df.iloc[i][:-1])
+            GEOID = df.iloc[i].GEOID_census_block_group
+            GEOID_ = address_to_census(address, aggregation="block groups")
+            assert GEOID == GEOID_, "GEOIDs do not match, {} and {}".format(
+                GEOID, GEOID_)
