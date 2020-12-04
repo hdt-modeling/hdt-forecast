f660221eab13eb7358496737e4dfab0b3af0f338
Stephen Sheng <stevexsheng@gmail.com>
2020-12-04 00:58:52 -0800

    added fix for addresses missing in 2010 census

--- hdt_util/tests/test_converter.py
+++ hdt_util/tests/test_converter.py
@@ -3,12 +3,12 @@ from hdt_util.converter import address_to_census
 
 
 def test_converter():
-    df = pd.read_csv("tests/test_data/CensusBlockGroupTest.csv")
+    df = pd.read_csv("tests/test_data/CensusBlockGroupTest.csv",dtype={'GEOID_census_block_group':str})
     df.Zip = df.Zip.astype(str)
+    #df['GEOID_census_block_group'] = df['GEOID_census_block_group'].astype(int)
     for i in range(len(df)):
-        if df.iloc[i].GEOID_census_block_group != "not found":
-            address = ",".join(df.iloc[i][:-1])
-            GEOID = df.iloc[i].GEOID_census_block_group
-            GEOID_ = address_to_census(address, aggregation="block groups")
-            assert GEOID == GEOID_, "GEOIDs do not match, {} and {}".format(
-                GEOID, GEOID_)
+        address = ",".join(df.iloc[i][:-1])
+        GEOID = df.iloc[i].GEOID_census_block_group
+        GEOID_ = address_to_census(address, aggregation="block groups")
+        assert GEOID == GEOID_, "GEOIDs do not match, {} and {}".format(
+            GEOID, GEOID_)
