3d8db5b9c9054e782cbe3c8fb26252012294f5fe
Stephen Sheng <stevexsheng@gmail.com>
2020-12-14 23:49:50 -0800

    added AR and ARLIC models

--- forecasters/forecasters/tests/test_autoregressive.py
+++ forecasters/forecasters/tests/test_autoregressive.py
@@ -1,12 +1,51 @@
+from forecasters.autoregressive._loss import mse
 from forecasters import AR
 import numpy as np
+import tensorflow as tf
 
 
-def test_AR():
+def test_AR_evaluation():
     model = AR(7)
     leading_indicator = np.random.uniform(100, 0, 1000)
-    cases = np.random.uniform(100, 0, 1000)
-    model.fit(leading_indicator)
-    model.fit_scale(leading_indicator, cases)
-    forecasts = model.forecast(leading_indicator, n=10)
-    assert len(forecasts) == 10
+    leading_indicator = tf.reshape(leading_indicator, shape=(1, -1, 1))
+    forecasts = model(leading_indicator)
+    forecasts = tf.reshape(forecasts, shape=-1)
+    assert len(forecasts) == 994
+
+
+def test_AR_forecast_method():
+    model = AR(7)
+    leading_indicator = np.random.uniform(100, 0, 1000)
+    leading_indicator = tf.reshape(leading_indicator, shape=(1, -1, 1))
+
+    forecasts = model.forecast(leading_indicator, n=1)
+    forecasts = tf.reshape(forecasts, shape=-1)
+    assert len(forecasts) == 1
+
+    forecasts = model.forecast(leading_indicator, n=2)
+    forecasts = tf.reshape(forecasts, shape=-1)
+    assert len(forecasts) == 2
+
+
+def test_AR_training():
+    model = AR(7)
+    optimizer = tf.keras.optimizers.Adam()
+    loss = mse
+
+    model.compile(
+        optimizer=optimizer,
+        loss=loss,
+    )
+
+    leading_indicator = np.random.uniform(100, 0, 1000)
+    leading_indicator = tf.reshape(leading_indicator, shape=(1, -1, 1))
+
+    reported_cases = np.random.uniform(100, 0, 1000)
+    reported_cases = tf.reshape(leading_indicator, shape=(1, -1, 1))
+
+    model.fit(
+        x=leading_indicator,
+        y=reported_cases,
+        epochs=10000,
+        verbose=0,
+    )
